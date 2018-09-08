6337a8af6fda885d3b0834e05a236a0c442d49a1
ninjacoder <cruisecoders@gmail.com>
2018-09-08 18:16:05 +0530

    no change

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
-var http = require('http');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World\n');
-}).listen(8080, '0.0.0.0');
-console.log('Server running at http://APP_SERVER_PRIVATE_IP_ADDRESS:8080/');
+const express = require('express');
+const path = require('path');
+/* eslint-disable no-console */
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+app.get('*.js', function(req, res, next) {
+ req.url = req.url.split('?')[0];
+ req.url = req.url + '.gz';
+ res.set('Content-Encoding', 'gzip');
+ res.set('Content-Type', 'text/javascript');
+ next();
+});
+
+app.get('*.css', function(req, res, next) {
+ req.url = req.url.split('?')[0];
+ req.url = req.url + '.gz';
+ res.set('Content-Encoding', 'gzip');
+ res.set('Content-Type', 'text/css');
+ next();
+});
+
+app.use(express.static('build'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, 'build/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) throw err;
+
+  console.log("Website running at port 3000")
+});
