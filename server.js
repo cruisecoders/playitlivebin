f810e26d5b75631f8a3d7b38c8999ca51affffcc
ninjacoder <cruisecoders@gmail.com>
2018-09-08 19:31:13 +0530

    Build issue

--- server.js
+++ server.js
@@ -5,23 +5,6 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-
-app.get('*.js', function(req, res, next) {
- req.url = req.url.split('?')[0];
- req.url = req.url + '.gz';
- res.set('Content-Encoding', 'gzip');
- res.set('Content-Type', 'text/javascript');
- next();
-});
-
-app.get('*.css', function(req, res, next) {
- req.url = req.url.split('?')[0];
- req.url = req.url + '.gz';
- res.set('Content-Encoding', 'gzip');
- res.set('Content-Type', 'text/css');
- next();
-});
-
 app.use(express.static('build'));
 
 app.get('*', function(req, res) {
